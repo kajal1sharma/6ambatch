f96fd5d467c8d69c4fc74d7242eda19a1048f1bc
kajal <kajal@gmail.com>
2024-05-02 06:52:17 +0530

    ..

---
+++ FirstProgram.java
@@ -0,0 +1,60 @@
+// java = everything will come inside a class
+
+// class test_875645one{
+
+// }
+
+class FirstProgram{
+
+
+    public static void main(String[] args) {
+
+        // variables are used to store the data 
+        // data => number, non numeric
+        // numeric => floating integral
+        // floating => 67.98,755.9776 (float, duoble)
+        // System.out.println(Float.MAX_VALUE);
+        // System.out.println(Double.MAX_VALUE);
+        // integral =8, -90,65... (byte , short, int , long)
+        // System.out.println(Byte.MAX_VALUE);
+
+        // keywords = reserved words = specific functionality = cannot be used as regular identifiers
+
+        /*identifiers = names of function, class, methods, variables, ....
+        *a-z A-Z
+        *$,_
+        0-9
+        numbers cannot be the first letter of identifiers
+        conventions
+        ===========
+        class, Interface name = first letter capital
+        variables,function =first letter small
+        enum, constant = All letter capital
+        camel case => firstNumber  , FirstNumber
+        */
+
+
+
+
+    }
+
+   
+   // public static void main(String[] args) {
+
+
+        // main entry point
+        //readability and documentation
+        /*
+         * this
+         * is
+         * a 
+         * multi 
+         * line 
+         * comment
+         */
+       // System.out.println("hello world");
+
+ //   }
+    
+
+}
