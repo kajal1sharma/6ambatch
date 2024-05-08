4d59a1c6c345d99e224d1ab775a685cc429e6530
kajal <kajal@gmail.com>
2024-05-08 06:58:31 +0530

    ..

--- Operators.java
+++ Operators.java
@@ -1,17 +1,49 @@
 public class Operators {
     public static void main(String[] args) {
-        
+       
+        // assignment operators 
+        int a= 10;
+        int b =20;
+        int c=30;
+
+        a+=b*c;
+
+        System.out.println(a);
+        //+= =>right to left 
+        // a = a +b;
+        // a += b ;
+        //a =a-b
+        // a -=b;
+        //a=a*b
+        // a *= b;
+        // a /= b;
+        // a %= b;
+        // System.out.println(a);
+        //  int result = a+b;
+
+
         //operators
 
         // airthmetic operators
         // + , -,*,/,%
 
-        int firstNum ;//declaration
-        firstNum =90;//initialization
+        // int firstNum ;//declaration
+        // firstNum =4;//initialization
 
-        int secondNum = 80;//dec + init
+        // int secondNum = 3;//dec + init
+        // // int thirdNum =2;
 
-        int result ;
+        // int result ;
+        // result = secondNum % firstNum;
+        // System.out.println(result);
+
+        // result = firstNum*secondNum/thirdNum/thirdNum;
+        // System.out.println(result);
+        // result = firstNum-secondNum*thirdNum;
+        // System.out.println(result);
+        // result =secondNum-firstNum-thirdNum;//left to right
+        // System.out.println(result);
+
     //    - ,*, /
 
         // result = firstNum+ secondNum; // = right side solved first
