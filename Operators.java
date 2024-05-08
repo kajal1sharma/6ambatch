b22f7aa714df3844206915db21da1890a2492a94
kajal <kajal@gmail.com>
2024-05-08 06:24:52 +0530

    ..

---
+++ Operators.java
@@ -0,0 +1,34 @@
+public class Operators {
+    public static void main(String[] args) {
+        
+        //operators
+
+        // airthmetic operators
+        // + , -,*,/,%
+
+        int firstNum ;//declaration
+        firstNum =90;//initialization
+
+        int secondNum = 80;//dec + init
+
+        int result ;
+    //    - ,*, /
+
+        // result = firstNum+ secondNum; // = right side solved first
+        // System.out.println(result);
+        
+        // write the code inside main function
+        
+        // single line comment
+
+        /*multi
+         line comment 
+         and 
+         it can upto n lines 
+         */
+
+
+
+        
+    }
+}
