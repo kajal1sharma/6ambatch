c42e943da3c3068319eb37c7960cfae79731cdbc
kajal <kajal@gmail.com>
2024-05-09 07:03:33 +0530

    ..

--- Operators2.java
+++ Operators2.java
@@ -2,21 +2,31 @@ public class Operators2 {
     public static void main(String[] args) {
 
 
+
+        boolean rain = true;
+        boolean umbrella = false;
+        boolean raincoat = false;
+        // is it rainig
+        // if it is raining then do i have an umbrella
+        // if it is raining then do i have a raincoat
+        // if it is raining then do i have a raincoat or umbrella 
+        System.out.println("sttate :  "+ (rain && (umbrella||raincoat )));
+
         // write a code where you are given with 3 numbers 
         // a =23, b=45, c=25 max 
         // output : a true/false
         // b true/false
         // c true/false
 
-        int a=23;
-        int b=45, c=25;
+        // int a=23;
+        // int b=45, c=25;
 
-        boolean result = a>b && a>c;
-        System.out.println("is a the greatest value : ??  " +result);
-        boolean result2 = b>a && b>c;
-        System.out.println("is b the greatest value : ??  " +result2);
-        boolean result3 = c>b && c>a;
-        System.out.println("is c the greatest value : ??  " +result3);
+        // boolean result = a>b && a>c;
+        // System.out.println("is a the greatest value : ??  " + result);
+        // boolean result2 = b>a && b>c;
+        // System.out.println("is b the greatest value : ??  " +result2);
+        // boolean result3 = c>b && c>a;
+        // System.out.println("is c the greatest value : ??  " +result3);
 
         // int b,c;
         // b=45;
