30da4877a90fdaf295b19e26d6a686db5cee20bc
kajal <kajal@gmail.com>
2024-05-09 06:33:32 +0530

    ..

---
+++ Operators2.java
@@ -0,0 +1,57 @@
+public class Operators2 {
+    public static void main(String[] args) {
+
+
+        // write a code where you are given with 3 numbers 
+        // a =23, b=45, c=25 max 
+        // output : a true/false
+        // b true/false
+        // c true/false
+
+        // logical operators
+        // && || !  input : boolean  output :boolean 
+        //  !operand !true= false , !false= true 
+        // operand1 + operand2  input:number output : number
+
+        //operand1 && operand2  =  answer
+        //   true     && true      =true
+        //   true  &&  false  = false
+        //   false && true = false
+        //   false && false = false
+
+        // int a= 45;
+        // int b=23;
+        // int c=90;
+        // boolean result1 = true || true;//optimization 
+        // boolean result2 = true || false;
+        // boolean result3 = false || false;
+        // boolean result4 = false || true;
+
+
+
+        // System.out.println(result1);
+        // System.out.println(result2);
+        // System.out.println(result3);
+        // System.out.println(result4);
+
+        // boolean result = !true;
+        // System.out.println(result );
+        // System.out.println(!false);
+
+
+
+        // conditionals operators
+        // >,<,<=,>=,==,!=  input : numbers  output : boolean
+        // boolean 
+        // strictly typed language
+        // int a=90;
+        // a=890;
+
+        // int a= 200;
+        // int b= 200;
+          
+        // boolean result1 = a!=b;
+        // System.out.println(result1);
+
+    }
+}
